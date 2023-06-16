d6b472fe69c511c9d89bb3d743f3345792b78ef0
toomtam12 <toomtam6640@gmail.com>
2023-06-16 15:11:43 +0700

    .commit

--- Home.py
+++ Home.py
@@ -16,3 +16,12 @@ with col2:
 with col3:
    st.header("Setosa")
    st.image("./pic/iris3.jpg")
+
+html_8 = """
+<div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+</div>
+"""
+
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
