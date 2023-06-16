3a559854265db608cd7b8a9e030d3c09d7cc6003
toomtam12 <toomtam6640@gmail.com>
2023-06-16 15:22:10 +0700

    .commit

--- Home.py
+++ Home.py
@@ -31,3 +31,8 @@ pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width"
 
 sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
 sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
+
+if st.button("ทำนายผล"):
+    st.write("ทำนาย")
+else:
+    st.write("ไม่ทำนาย")
