0019ef7e912d86a98f4160a487a1840042e1810d
toomtam12 <toomtam6640@gmail.com>
2023-06-16 15:59:19 +0700

    .commit

--- Home.py
+++ Home.py
@@ -32,9 +32,24 @@ st.markdown("")
 dt = pd.read_csv("./data/iris.csv")
 st.write(dt.head(10))
 
+t2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+   #st.write(dt.head(10))
+   st.bar_chart(dx2)
+   st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
+
 
 html_8 = """
-<div style="background-color:#CC3333;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:#6666FF;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>ทำนายข้อมูล</h5></center>
 </div>
 """
