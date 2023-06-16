53dfd086879933012644731bee606300c475d261
toomtam12 <toomtam6640@gmail.com>
2023-06-16 15:15:25 +0700

    .commit

--- Home.py
+++ Home.py
@@ -19,9 +19,15 @@ with col3:
 
 html_8 = """
 <div style="background-color:#FFCCCC;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+<center><h5>ทำนายข้อมูลดอกไม้</h5></center>
 </div>
 """
 
 st.markdown(html_8, unsafe_allow_html=True)
 st.markdown("")
+
+pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
+pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
+
+sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
+sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
