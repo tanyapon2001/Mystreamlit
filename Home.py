d3832f4c541cd46e42a0ba7fdf1737b2d80f652f
toomtam12 <toomtam6640@gmail.com>
2023-06-16 14:56:38 +0700

    .commit

--- Home.py
+++ Home.py
@@ -2,4 +2,16 @@ import streamlit as st
 
 
 st.header('therdsak')
+col1, col2, col3 = st.columns(3)
 
+with col1:
+   st.header("Versicolor")
+   st.image("./pic/iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./pic/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./pic/iris3.jpg")
